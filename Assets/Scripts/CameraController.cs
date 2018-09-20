7bf58b14b4205fbcf932682d577d6d9aabb303a2
lblumhardt <lucas.blumhardt.96@gmail.com>
2018-09-19 19:44:24 -0700

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private float offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //distance from camera to player
+        offset = 3.0f;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+
+        transform.position = player.transform.position - (player.transform.forward * offset) + new Vector3(0.0f, 1.0f, 0.0f);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float turningConstant = 200.0f;
+    float speedConstant = 6.5f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turningConstant;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speedConstant;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
